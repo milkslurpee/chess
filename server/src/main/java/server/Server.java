8d3ba50115837c9f395837439bd42e2f9b521522
Jack_Suorsa <johnsuorsa@gmail.com>
2024-05-27 21:28:54 -0700

    changed all endpoint definitions to the type that Makenna (TA) suggested.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -23,13 +23,13 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
-        Spark.delete("/db", new ClearDbHandler(new ClearService(authDAO, userDAO, gameDAO), gson)::handleClear);
-        Spark.post("/user", new RegisterHandler(new RegisterService(authDAO, userDAO, gameDAO), gson)::handleRegister);
-        Spark.post("/session", new LoginHandler(new LoginService(authDAO, userDAO, gameDAO), gson)::handleLogin);
+        Spark.delete("/db", (request, response) -> new ClearDbHandler(new ClearService(authDAO, userDAO, gameDAO), gson).handleClear(request, response));
+        Spark.post("/user", (request, response) -> new RegisterHandler(new RegisterService(authDAO, userDAO, gameDAO), gson).handleRegister(request, response));
+        Spark.post("/session", (request, response) -> new LoginHandler(new LoginService(authDAO, userDAO, gameDAO), gson).handleLogin(request, response));
         Spark.delete("/session", (request, response) -> new LogoutHandler(new LogoutService(authDAO, userDAO), gson).handleLogout(request, response));
-        Spark.get("/game", new ListGamesHandler(new ListGameService(gameDAO), gson)::handleList);
-        Spark.post("/game", new CreateGameHandler(new CreateGameService(authDAO, userDAO, gameDAO), gson)::handleCreate);
-        Spark.put("/game", new JoinGameHandler(new JoinGameService(authDAO, userDAO, gameDAO), gson)::handleJoin);
+        Spark.get("/game", (request, response) -> new ListGamesHandler(new ListGameService(gameDAO), gson).handleList(request, response));
+        Spark.post("/game", (request, response) -> new CreateGameHandler(new CreateGameService(authDAO, userDAO, gameDAO), gson).handleCreate(request, response));
+        Spark.put("/game", (request, response) -> new JoinGameHandler(new JoinGameService(authDAO, userDAO, gameDAO), gson).handleJoin(request, response));
 
         // Register your endpoints and handle exceptions here.
 
