30a1571117785eecdca0890cb5291a0148b487e9
Jack_Suorsa <johnsuorsa@gmail.com>
2024-05-28 22:44:27 -0700

    decomposing code

--- shared/src/main/java/chess/Bishop.java
+++ shared/src/main/java/chess/Bishop.java
@@ -16,30 +16,4 @@ public class Bishop extends ChessPiece{
         return generateMoves(board, myPosition, directions);
     }
 
-    protected Collection<ChessMove> generateMoves(ChessBoard board, ChessPosition myPosition, int[][] directions) {
-        Set<ChessMove> validMoves = new HashSet<>();
-        int row = myPosition.getRow();
-        int column = myPosition.getColumn();
-
-        for (int[] direction : directions) {
-            int newRow = row + direction[0];
-            int newCol = column + direction[1];
-
-            while (newRow > 0 && newRow < 9 && newCol > 0 && newCol < 9) {
-                ChessPosition newPosition = new ChessPosition(newRow, newCol);
-                ChessPiece newPiece = board.getPiece(newPosition);
-
-                if (newPiece == null || newPiece.getTeamColor() != this.teamColor) {
-                    validMoves.add(new ChessMove(myPosition, newPosition));
-                }
-
-                if (newPiece != null) {
-                    break;
-                }
-                newRow += direction[0];
-                newCol += direction[1];
-            }
-        }
-        return validMoves;
-    }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -116,6 +116,34 @@ public class ChessPiece {
         return teamColor == that.teamColor && pieceType == that.pieceType;
     }
 
+
+    protected Collection<ChessMove> generateMoves(ChessBoard board, ChessPosition myPosition, int[][] directions) {
+        Set<ChessMove> validMoves = new HashSet<>();
+        int row = myPosition.getRow();
+        int column = myPosition.getColumn();
+
+        for (int[] direction : directions) {
+            int newRow = row + direction[0];
+            int newCol = column + direction[1];
+
+            while (newRow > 0 && newRow < 9 && newCol > 0 && newCol < 9) {
+                ChessPosition newPosition = new ChessPosition(newRow, newCol);
+                ChessPiece newPiece = board.getPiece(newPosition);
+
+                if (newPiece == null || newPiece.getTeamColor() != this.teamColor) {
+                    validMoves.add(new ChessMove(myPosition, newPosition));
+                }
+
+                if (newPiece != null) {
+                    break;
+                }
+                newRow += direction[0];
+                newCol += direction[1];
+            }
+        }
+        return validMoves;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(teamColor, pieceType);
