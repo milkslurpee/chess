21404c94f8576957683c47780ad470e60a9be7c8
Jack_Suorsa <johnsuorsa@gmail.com>
2024-06-13 04:48:54 -0600

    wrote register method in serverFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -9,12 +9,15 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
+    String existingAuth;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
