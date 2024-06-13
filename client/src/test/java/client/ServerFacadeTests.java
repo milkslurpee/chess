4d1fd216648fd30257f2822ca05e50ff2fab162f
Jack_Suorsa <johnsuorsa@gmail.com>
2024-06-13 17:18:25 -0600

    bing bong

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,7 +3,13 @@ package client;
 import chess.ChessGame;
 import org.junit.jupiter.api.*;
 import facade.ServerFacade;
+import requests.*;
+import responses.*;
 import server.Server;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
@@ -18,6 +24,26 @@ public class ServerFacadeTests {
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    public void setup() throws IOException {
+        // facade.clearServer(); //need to clear the database between each test
+
+        //one user already logged in
+        RegisterResponse userResponse = facade.register(new RegisterRequest("user", "pass", "email"));
+        existingAuth = userResponse.getAuthToken();
+    }
+
+    @Test
+    @DisplayName("Register Works")
+    public void registerWorks() throws Exception {
+
+        RegisterResponse regResp = facade.register(new RegisterRequest("user1", "pass", "email"));
+        String auth1 = regResp.getAuthToken();
+        assertTrue(auth1.length() > 10, "Auth token should be longer than 10 characters");
+        assertEquals("user1", regResp.getUsername(), "Should return the same username");
+        Assertions.assertNotEquals(existingAuth, auth1, "Auth tokens should be different");
     }
 
     @AfterAll
